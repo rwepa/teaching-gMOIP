e0af87708d4c38a7dafa1d355788cd3e6e392893
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2026-04-18 22:52:51 +0800

    Create r_gMOIP_maximum_profit.R

---
+++ r_gMOIP_maximum_profit.R
@@ -0,0 +1,61 @@
+# File     : r_gMOIP_maximum_profit.R
+# Title    : gMOIP套件最大獲利範例
+# Author   : Ming-Chang Lee
+# Email    : alan9956@gmail.com
+# RWEPA    : http://rwepa.blogspot.tw/
+# GitHub   : https://github.com/rwepa
+
+# gMOIP CRAN:
+# https://cran.r-project.org/web/packages/gMOIP/index.html
+
+# 安裝 gMOIP 套件
+install.packages("gMOIP")
+
+# R 套件安裝目錄
+libPaths()
+
+# 載入套件
+library(gMOIP)
+
+# materials: https://github.com/rwepa/teaching-gMOIP/blob/main/r_gMOIP_tutorial.pdf
+
+# 建立限制條件矩陣
+A <- matrix(c(1, 0, 0, 2, 3, 2), ncol=2, byrow = TRUE)
+A
+
+# 建立限制條件
+b <- c(4, 12, 18)
+b
+
+# 建立目標函數
+obj <- c(300, 500)
+obj
+
+# 繪製凸集(Convex set)線上說明
+?plotPolytope
+
+# case 1. 繪製凸集(Convex set)
+plotPolytope(
+  A,
+  b,
+  obj
+)
+
+# case 2. 繪製凸集(Convex set)-加入最佳值
+plotPolytope(
+  A,
+  b,
+  obj,
+  plotOptimum = TRUE
+)
+
+# case 3. 繪製凸集(Convex set)-加入最佳值,最佳解+極點(Extreme Point)
+plotPolytope(
+  A,
+  b,
+  obj,
+  crit = "max",
+  plotOptimum = TRUE,
+  labels = "coord"
+)
+# end
